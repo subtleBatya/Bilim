3b736ff53363c10d48314fa37ad8714f64c95345
subtleBatya <batyrsazakow.2003@gmail.com>
2024-12-18 08:56:55 +0300

    udpate

--- scripts/script.js
+++ scripts/script.js
@@ -29,34 +29,29 @@ function removeAnimationClasses(element, animationClass) {
 }
 
 function updateContent() {
-  const imageElement = document.getElementById('changing-image');
-  const titleElement = document.getElementById('changing-title');
-  const textElement = document.getElementById('changing-text');
+  const elements = [
+    document.getElementById('changing-image'),
+    document.getElementById('changing-title'),
+    document.getElementById('changing-text')
+  ];
 
-  imageElement.classList.add('fade-out');
-  titleElement.classList.add('fade-out');
-  textElement.classList.add('fade-out');
+  elements.forEach(element => element.classList.add('fade-out'));
 
   setTimeout(() => {
-    imageElement.src = data[currentIndex].image;
-    titleElement.textContent = data[currentIndex].title;
-    textElement.textContent = data[currentIndex].text;
+    elements[0].src = data[currentIndex].image;
+    elements[1].textContent = data[currentIndex].title;
+    elements[2].textContent = data[currentIndex].text;
 
-    imageElement.classList.add('fade-in');
-    titleElement.classList.add('fade-in');
-    textElement.classList.add('fade-in');
-
-    removeAnimationClasses(imageElement, 'fade-in');
-    removeAnimationClasses(titleElement, 'fade-in');
-    removeAnimationClasses(textElement, 'fade-in');
-
-    removeAnimationClasses(imageElement, 'fade-out');
-    removeAnimationClasses(titleElement, 'fade-out');
-    removeAnimationClasses(textElement, 'fade-out');
+    elements.forEach(element => {
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+      removeAnimationClasses(element, 'fade-in');
+    });
 
     currentIndex = (currentIndex + 1) % data.length;
   }, 500);
 }
+
 
 updateContent();
 
@@ -66,68 +61,68 @@ setInterval(updateContent, 5000);
 const header = document.querySelector('#navbar');
 
 function changeHeaderColor() {
-    const scrollPosition = window.scrollY; 
-    const windowHeight = document.documentElement.scrollHeight - window.innerHeight;
-    const scrollThreshold = windowHeight * 0.1; 
+  const scrollPosition = window.scrollY;
+  const windowHeight = document.documentElement.scrollHeight - window.innerHeight;
+  const scrollThreshold = windowHeight * 0.1;
 
-    if (scrollPosition > scrollThreshold) {
-        header.classList.remove('bg-transparent');
-        header.style.backgroundColor = 'rgba(178, 235, 242, 1)';
-    } else {
-        header.classList.add('bg-transparent');
-    }
+  if (scrollPosition > scrollThreshold) {
+    header.classList.remove('bg-transparent');
+    header.style.backgroundColor = 'rgba(178, 235, 242, 1)';
+  } else {
+    header.classList.add('bg-transparent');
+  }
 }
 window.addEventListener('scroll', changeHeaderColor);
 
 
 // Функция для стрелок меню
 document.addEventListener('DOMContentLoaded', () => {
-	const profileDropdown = document.getElementById('profileDropdown')
-	const profileMenu = document.getElementById('profileMenu')
-	const coursesDropdown = document.getElementById('coursesDropdown')
-	const coursesMenu = document.getElementById('coursesMenu')
+  const profileDropdown = document.getElementById('profileDropdown')
+  const profileMenu = document.getElementById('profileMenu')
+  const coursesDropdown = document.getElementById('coursesDropdown')
+  const coursesMenu = document.getElementById('coursesMenu')
 
-	function toggleMenu(dropdown, menu) {
-		const arrow = dropdown.querySelector('.arrow') 
-		const isVisible = menu.classList.contains('show')
+  function toggleMenu(dropdown, menu) {
+    const arrow = dropdown.querySelector('.arrow')
+    const isVisible = menu.classList.contains('show')
 
-		closeAllMenus()
+    closeAllMenus()
 
-		if (!isVisible) {
-			menu.classList.add('show')
-			arrow.textContent = '▲' 
-			arrow.style.color = '#FFA000' 
-		} else {
-			arrow.textContent = '▼' 
-			arrow.style.color = 'black' 
-		}
-	}
+    if (!isVisible) {
+      menu.classList.add('show')
+      arrow.textContent = '▲'
+      arrow.style.color = '#FFA000'
+    } else {
+      arrow.textContent = '▼'
+      arrow.style.color = 'black'
+    }
+  }
 
-	function closeAllMenus() {
-		document.querySelectorAll('.dropdown-menu').forEach(menu => {
-			menu.classList.remove('show')
-		})
-		document.querySelectorAll('.arrow').forEach(arrow => {
-			arrow.textContent = '▼' 
-			arrow.style.color = 'black' 
-		})
-	}
+  function closeAllMenus() {
+    document.querySelectorAll('.dropdown-menu').forEach(menu => {
+      menu.classList.remove('show')
+    })
+    document.querySelectorAll('.arrow').forEach(arrow => {
+      arrow.textContent = '▼'
+      arrow.style.color = 'black'
+    })
+  }
 
-	profileDropdown.addEventListener('click', e => {
-		e.preventDefault()
-		toggleMenu(profileDropdown, profileMenu)
-	})
+  profileDropdown.addEventListener('click', e => {
+    e.preventDefault()
+    toggleMenu(profileDropdown, profileMenu)
+  })
 
-	coursesDropdown.addEventListener('click', e => {
-		e.preventDefault()
-		toggleMenu(coursesDropdown, coursesMenu)
-	})
+  coursesDropdown.addEventListener('click', e => {
+    e.preventDefault()
+    toggleMenu(coursesDropdown, coursesMenu)
+  })
 
-	document.addEventListener('click', e => {
-		if (!e.target.closest('.nav-item')) {
-			closeAllMenus()
-		}
-	})
+  document.addEventListener('click', e => {
+    if (!e.target.closest('.nav-item')) {
+      closeAllMenus()
+    }
+  })
 })
 
 
