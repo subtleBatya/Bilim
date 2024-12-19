fb5a3cdbb5ddf125c760dd18fcdaa6a463d4a1bc
subtleBatya <batyrsazakow.2003@gmail.com>
2024-12-19 17:17:48 +0500

    update

--- scripts/script.js
+++ scripts/script.js
@@ -94,3 +94,33 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
+document.addEventListener("DOMContentLoaded", () => {
+  const player = videojs('my-video', {
+    autoplay: false, 
+    controls: true, 
+    responsive: true,
+    fluid: true, 
+  });
+
+  
+  player.on('play', () => {
+    console.log('Video started playing!');
+  });
+
+  player.on('pause', () => {
+    console.log('Video paused.');
+  });
+
+  player.on('ended', () => {
+    console.log('Video ended.');
+  });
+
+  
+  document.getElementById("change-video-btn").addEventListener("click", () => {
+    player.src({
+      src: "path-to-another-video.mp4",
+      type: "video/mp4",
+    });
+    player.play();
+  });
+});
