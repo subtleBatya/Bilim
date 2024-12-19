80916c4a757d6fd51647ade788dc25cda5573b72
subtleBatya <batyrsazakow.2003@gmail.com>
2024-12-19 18:09:49 +0500

    added video

--- scripts/script.js
+++ scripts/script.js
@@ -69,35 +69,4 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 
-// VIDEO player
-document.addEventListener("DOMContentLoaded", () => {
-  const player = videojs('my-video', {
-    autoplay: false, 
-    controls: true, 
-    responsive: true,
-    fluid: true, 
-    playbackRates: [0.5, 1, 1.5, 2],
-  });
 
-  
-  player.on('play', () => {
-    console.log('Video started playing!');
-  });
-
-  player.on('pause', () => {
-    console.log('Video paused.');
-  });
-
-  player.on('ended', () => {
-    console.log('Video ended.');
-  });
-
-  
-  document.getElementById("change-video-btn").addEventListener("click", () => {
-    player.src({
-      src: "path-to-another-video.mp4",
-      type: "video/mp4",
-    });
-    player.play();
-  });
-});
