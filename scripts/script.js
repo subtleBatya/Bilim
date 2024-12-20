e30db2db842a2a6db1594711c144975eba8453fc
guga <sashakovalev9911@gmail.com>
2024-12-21 02:20:57 +0500

    payment

--- scripts/script.js
+++ scripts/script.js
@@ -68,8 +68,7 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 })
 
-
- function changeLanguage() {
+function changeLanguage() {
   
   let languageName = document.getElementById('languageName');
   let languageImage = document.getElementById("languageImage");
@@ -89,3 +88,26 @@ document.addEventListener('DOMContentLoaded', () => {
   languageImage.src = imageSrc;
   console.log("Language path is set to:", imageSrc);
 }
+
+//Функция для button в Оплате
+const button1 = document.getElementById('button1')
+const button2 = document.getElementById('button2')
+const content = document.getElementById('content')
+const registration = document.getElementById('registration')
+
+content.classList.remove('d-none')
+button1.classList.add('active')
+
+button1.addEventListener('click', () => {
+content.classList.remove('d-none')
+registration.classList.add('d-none')
+button1.classList.add('active')
+button2.classList.remove('active')
+})
+
+button2.addEventListener('click', () => {
+content.classList.add('d-none')
+registration.classList.remove('d-none')
+button2.classList.add('active')
+button1.classList.remove('active')
+})
