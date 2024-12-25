cc4f486e7a075286fce47749abb86cd3608811d6
subtleBatya <batyrsazakow.2003@gmail.com>
2024-12-25 17:20:43 +0500

    udpate

--- scripts/monitoring.js
+++ scripts/monitoring.js
@@ -4,15 +4,18 @@ let gen_btn = document.getElementById("gen_btn");
 let exam_btn = document.getElementById("exam_btn");
 let class_btn = document.getElementById("class_btn");
 
+
+let atdnc_block = document.getElementById("attendance_block");
 let gen_block = document.getElementById("general_block");
 let exam_block = document.getElementById("exams_block");
 let class_block = document.getElementById("class_block");
 
-
+atdnc_block.classList.add('d-none');
 
 exam_btn.addEventListener( "click", () => {
     exam_btn.classList.add('border-bottom', 'border-2', 'border-primary');
     exam_block.classList.remove('d-none');
+    atdnc_block.classList.remove('d-none');
     
     if(exam_btn.classList.contains('border-bottom', 'border-2', 'border-primary')) {
         gen_btn.classList.remove('border-bottom', 'border-2', 'border-primary');
@@ -26,7 +29,8 @@ exam_btn.addEventListener( "click", () => {
 gen_btn.addEventListener( "click", () => {
     gen_btn.classList.add('border-bottom', 'border-2', 'border-primary');
     gen_block.classList.remove('d-none');
-    
+    atdnc_block.classList.add('d-none');
+
     if(gen_btn.classList.contains('border-bottom', 'border-2', 'border-primary')){
         exam_btn.classList.remove('border-bottom', 'border-2', 'border-primary');
         class_btn.classList.remove('border-bottom', 'border-2', 'border-primary');
@@ -38,6 +42,7 @@ gen_btn.addEventListener( "click", () => {
 class_btn.addEventListener("click", () => {
     class_btn.classList.add('border-bottom', 'border-2', 'border-primary');
     class_block.classList.remove('d-none');
+    atdnc_block.classList.remove('d-none');
 
     if(class_btn.classList.contains('border-bottom', 'border-2', 'border-primary')){
         exam_btn.classList.remove('border-bottom', 'border-2', 'border-primary');
