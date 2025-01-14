0572c39fffd9e33a272d4ce463c6cbbb57e8f819
CodeAssasinKing <bayramowramazan85@gmail.com>
2025-01-14 18:01:14 +0500

    added teacher profile

--- Bilim/auth/views.py
+++ Bilim/auth/views.py
@@ -17,3 +17,8 @@ def payment(request):
 
 def teacher_profile(request):
     return render(request, "core/teacher_profile.html")
+
+
+
+def teacher_video_create(request):
+    return render(request, "core/teacher_create_video.html")
