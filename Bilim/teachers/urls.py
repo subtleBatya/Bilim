4b22068857448057f2ca213d5cd6aeba8350f5d7
CodeAssasinKing <bayramowramazan85@gmail.com>
2025-01-24 18:30:50 +0500

    added footer to all pages

--- Bilim/teachers/urls.py
+++ Bilim/teachers/urls.py
@@ -6,7 +6,7 @@ app_name = "teacher"
 
 urlpatterns = [
     path("", teacher_profile, name="teacher_profile"),
-    path("content/", teacher_content, name="teacher_profile"),
+    path("content/", teacher_content, name="teacher_content"),
     path("create/", teacher_video_create, name="teacher_video_create"),
     path("edit/", teacher_edit, name="teacher_edit")
 ]
