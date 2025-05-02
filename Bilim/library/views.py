300d2b75b67bd26b041ee658a0b9c62c5963d090
CodeAssasinKing <bayramowramazan85@gmail.com>
2025-05-02 18:51:58 +0500

    infinite scrolling for library

--- Bilim/library/views.py
+++ Bilim/library/views.py
@@ -3,31 +3,48 @@ from django.contrib.auth.decorators import login_required
 from .models import Book, BookCategory
 from django.shortcuts import get_object_or_404
 from videos.models import Video_category
+from django.core.paginator import Paginator, EmptyPage
+from django.http import JsonResponse
+from django.template.loader import render_to_string
 @login_required
 def library(request):
-    if request.method == "GET":
-        book_name = request.GET.get("book_name")
-        category_name = request.GET.get("category")
-        courses = Video_category.objects.all()
-        books = Book.objects.all()
-        categories = BookCategory.objects.all()
-    
-        if book_name and category_name:
-            books = Book.objects.filter(name__icontains=book_name, category__category_name=category_name)
-        elif book_name and category_name is not None:
-            books = Book.objects.filter(name__icontains=book_name)
-        elif book_name is not None and category_name:
-            books = Book.objects.filter(category__category_name=category_name)
+    book_name = request.GET.get("book_name")
+    category_name = request.GET.get("category")
 
-        context = {
-                "books": books,
-                "categories": categories,
-                "book_name":book_name,
-                "category_name": category_name,
-                "courses":courses
-            }
-        print(categories)
-        return render(request, "core/library.html", context)
+    courses = Video_category.objects.all()
+    categories = BookCategory.objects.all()
+    books = Book.objects.all()
+
+    if book_name and category_name:
+        books = books.filter(name__icontains=book_name, category__category_name=category_name)
+    elif book_name:
+        books = books.filter(name__icontains=book_name)
+    elif category_name:
+        books = books.filter(category__category_name=category_name)
+
+    paginator = Paginator(books, 9)
+    page_number = int(request.GET.get("page", 1))
+
+    try:
+        page_obj = paginator.page(page_number)
+    except EmptyPage:
+        page_obj = []
+
+    if request.headers.get("x-requested-with") == "XMLHttpRequest":
+        html = render_to_string("core/partials/book_cards.html", {"books": page_obj}, request=request)
+        return JsonResponse({
+            "html": html,
+            "has_next": page_obj.has_next() if hasattr(page_obj, 'has_next') else False
+        })
+
+    context = {
+        "books": page_obj,
+        "categories": categories,
+        "book_name": book_name,
+        "category_name": category_name,
+        "courses": courses
+    }
+    return render(request, "core/library.html", context)
         
 
 
